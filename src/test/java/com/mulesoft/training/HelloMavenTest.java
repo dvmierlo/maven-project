8db37e75fb1e8c5d109b9df97ff79a82bfe45452
Dennis van Mierlo <dvanmierlo@datacon.nl>
2018-06-14 16:37:31 +0200

    Added content type test for flights api.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,14 +3,25 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase
 {
     @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception
+    public void mavenFlow_ReturnsHelloMaven() throws Exception
     {
         runFlowAndExpect("mavenFlow", "Hello Maven");
+    }
+    
+    @Test
+    public void retrieveFlights_ContextType() throws Exception
+    {
+    	String expectedResult = "application/json";
+    	
+    	MuleEvent event = runFlow("retrieveFlights");
+    	String result = event.getMessage().getOutboundProperty("Content-Type");
+    	assertEquals(expectedResult, result);
     }
     
     @Override
